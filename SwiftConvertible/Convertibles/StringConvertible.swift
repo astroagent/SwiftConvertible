44f32febc1ab39c8d3df449874e60c4c90fca181
橋本 龍 <hashimotoryoh@gmail.com>
2019-04-14 11:15:25 +0900

    LosslessStringConvertible に StringConvertible を生やし、Int に準拠されてユニットテストも書いた

---
+++ SwiftConvertible/Convertibles/StringConvertible.swift
@@ -0,0 +1,35 @@
+//
+//  StringConvertible.swift
+//  SwiftConvertible
+//
+//  Created by 橋本 龍 on 2019/04/14.
+//  Copyright © 2019 Ryoh Hashimoto. All rights reserved.
+//
+
+import Foundation
+
+public protocol StringConvertible {
+    func toString() throws -> String
+}
+
+public extension StringConvertible {
+    func asString() -> String? {
+        return try? self.toString()
+    }
+}
+
+extension String: StringConvertible {
+    public func toString() -> String {
+        return self
+    }
+}
+
+
+// MARK: Converting
+
+extension LosslessStringConvertible where Self: StringConvertible {
+    public func toString() -> String {
+        return String(self)
+    }
+}
+extension Int: StringConvertible {}
---
+++ SwiftConvertibleTests/Convertibles/StringConvertible/String+Int.swift
@@ -0,0 +1,36 @@
+//
+//  String+LosslessStringConvertible.swift
+//  SwiftConvertibleTests
+//
+//  Created by 橋本 龍 on 2019/04/14.
+//  Copyright © 2019 Ryoh Hashimoto. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftConvertible
+
+class StringConvertibleFromIntTests: XCTestCase {
+
+    // MARK: - 正常系
+
+    /// 正の数が変換可能
+    func testPositiveNumberConvertible() {
+        let int: Int = 1
+        XCTAssertEqual(int.toString(), "1")
+    }
+
+    /// 負の数が変換可能
+    func testNegativeNumberConvertible() {
+        let int: Int = -1
+        XCTAssertNoThrow(int.toString())
+        XCTAssertEqual(int.toString(), "-1")
+    }
+
+    /// `0`が変換可能
+    func testZeroConvertible() {
+        let int: Int = 0
+        XCTAssertNoThrow(int.toString())
+        XCTAssertEqual(int.toString(), "0")
+    }
+
+}
