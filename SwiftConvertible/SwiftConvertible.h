a42074ce5b2dc0bc42a8de7905d78cc92ea2f2c3
橋本 龍 <hashimotoryoh@gmail.com>
2019-04-14 10:51:20 +0900

    create new cocoa framework project

---
+++ SwiftConvertible/SwiftConvertible.h
@@ -0,0 +1,19 @@
+//
+//  SwiftConvertible.h
+//  SwiftConvertible
+//
+//  Created by 橋本 龍 on 2019/04/14.
+//  Copyright © 2019 Ryoh Hashimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftConvertible.
+FOUNDATION_EXPORT double SwiftConvertibleVersionNumber;
+
+//! Project version string for SwiftConvertible.
+FOUNDATION_EXPORT const unsigned char SwiftConvertibleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftConvertible/PublicHeader.h>
+
+
